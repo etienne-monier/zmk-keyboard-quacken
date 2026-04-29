2f4ed42f1cf14a2b8535f4d8d98ba7f0a819daf0
Etienne Monier <etienne.monier@cs-soprasteria.com>
2026-04-29 21:12:35 +0200

    chore: Configure the base settings for my config

--- keymaps/settings.h
+++ keymaps/settings.h
@@ -1,10 +1,9 @@
-// By default, the firmware will be built with HRM support for a QWERTY computer.
-// Uncomment definitions in this file to activate options.
-
+// By default, the firmware will be built with HRM support for a QWERTY
+// computer. Uncomment definitions in this file to activate options.
 
 /******************************************************************************
  * Keyboard Layout
-******************************************************************************/
+ ******************************************************************************/
 
 // Uncomment one of the following lines if using a non-QWERTY layout:
 // (this sets an appropriate Symbols layer and relevant keyboard shortcuts)
@@ -13,7 +12,7 @@
 // #define KB_LAYOUT_BEPO
 // #define KB_LAYOUT_BEPOLAR
 // #define KB_LAYOUT_DVORAK
-// #define KB_LAYOUT_ERGOL
+#define KB_LAYOUT_ERGOL
 // #define KB_LAYOUT_ERGLACE
 // #define KB_LAYOUT_QWERTY_INTL
 // #define KB_LAYOUT_QWERTY_LAFAYETTE
@@ -22,14 +21,15 @@
 // Uncomment one of the following lines if using a non-Windows host:
 
 // #define MACOS
-// #define LINUX
+#define LINUX
 
 // [Experimental]
 // Uncomment one of the following lines to enable layout emulation:
 
 // #define KB_EMULATION_DVORAK           // assumes the host is in QWERTY
-// #define KB_EMULATION_ERGOL            // assumes the host is in QWERTY-intl or AZERTY
-// #define KB_EMULATION_QWERTY_LAFAYETTE // assumes the host is in QWERTY-intl or AZERTY
+// #define KB_EMULATION_ERGOL            // assumes the host is in QWERTY-intl
+// or AZERTY #define KB_EMULATION_QWERTY_LAFAYETTE // assumes the host is in
+// QWERTY-intl or AZERTY
 
 // [Experimental]
 // Uncomment the following line for an improved dead key support;
@@ -37,46 +37,44 @@
 
 // #define ENABLE_FANCY_DEAD_KEYS
 
-
 /******************************************************************************
  * Hold-Taps
-******************************************************************************/
+ ******************************************************************************/
 
 // Uncomment one of the following lines to pick your preferred hold-tap config:
 
 // #define HT_NONE
 // #define HT_THUMB_TAPS
-// #define HT_HOME_ROW_MODS  // (default behavior)
+#define HT_HOME_ROW_MODS // (default behavior)
 // #define HT_TWO_THUMB_KEYS
 
 // Timing is key! Keep the default value if unsure.
 // This defines how long (in ms) a hold-tap key with the "tap-preferred" flavor
 // must be held to be considered as a modifier or layer shift. These hold-taps
-// are the ones that may produce text, including the space bar and home row mods.
-// Keep this value high if you struggle with home row mods.
+// are the ones that may produce text, including the space bar and home row
+// mods. Keep this value high if you struggle with home row mods.
 
-// #define TAPPING_TERM 300
+#define TAPPING_TERM 300
 
 // This defines how quickly (in ms) you need to press and release a hold-tap
 // with the "hold-preferred" flavor for it to be considered a tap. These
 // hold-taps include the ones that don’t produce text when tapped.
 // Keep this value low if you press Enter or Escape by mistake too often.
 
-// #define SHORT_TAPPING_TERM 150
+#define SHORT_TAPPING_TERM 150
 
 // When tapping then holding the same tap-hold, if the delay between the two
 // key presses is lower than `QUICK_TAP`, the tap-hold will hold the tap action
 // instead of the hold action. Again, keep the default value if unsure.
 
-// #define QUICK_TAP 200
+#define QUICK_TAP 200
 
 // For more information on how hold-taps operate, feel free to read ZMK’s docs:
 // https://zmk.dev/docs/keymaps/behaviors/hold-tap
 
-
 /******************************************************************************
  * Other Options
-*****************************************************************************/
+ *****************************************************************************/
 
 // Uncomment the following line to split the NavNum layer in two:
 //  - one layer for vim-style navigation (right) and GUI shortcuts (left)
@@ -84,13 +82,14 @@
 //  + Escape under the left thumb (direct access)
 // Highly recommended for Vim users, obviously. :-)
 
-// #define VIM_NAVIGATION
+#define VIM_NAVIGATION
 
 // [Experimental]
 // Uncomment the following line to enable the "mod-hold behavior" on the left
 // hand’s navigation layer key. Enabling this means that if the Alt key is held
 // when entering the navigation layer, Alt will only be released when nav is
-// released. This enables one-handed alt-tabs, but may cause unwanted side effects.
+// released. This enables one-handed alt-tabs, but may cause unwanted side
+// effects.
 
 // #define ENABLE_MOD_HOLD_NAVIGATION
 
